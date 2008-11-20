c44010e1a681280ed95c0dff410d7ed287a55049
Ben Brinckerhoff <ben@devver.net>
2008-11-19 19:33:00 -0700

    Made font really big for easier reading on iphone

--- iphone_doc.rb
+++ iphone_doc.rb
@@ -1,19 +1,23 @@
+#! /usr/bin/env ruby
+
 require 'sinatra'
 
 get "/" do
   dir = "./public/"
-  out = ""
+  bigness = 5
+  out = "<big>"*bigness
+  out += "<h3 style='+3'>your files</h3>"
   Dir[dir+"*"].each do |file| 
     out+=make_link(dir,file)
   end
-  out
+  out+="</big>"*bigness
 end
 
 helpers do
 
   def make_link(dir,file)
     relative = file.gsub(dir,"")
-    "<a href='#{relative}'>#{relative}</a><br/>"
+    "<a href='#{relative}'>#{relative}</a><br/><br/>"
   end
   
 end

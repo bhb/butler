50bc756f859fc67bbc06fc8f1c490a20dfd96abd
Ben Brinckerhoff <ben@devver.net>
2008-11-19 19:08:33 -0700

    Now it displays a link to each file

--- iphone_doc.rb
+++ iphone_doc.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
 get "/" do
-  "Hello world"
+  dir = "./public/"
+  out = ""
+  Dir[dir+"*"].each do |file| 
+    out+=make_link(dir,file)
+  end
+  out
 end
+
+helpers do
+
+  def make_link(dir,file)
+    relative = file.gsub(dir,"")
+    "<a href='#{relative}'>#{relative}</a><br/>"
+  end
+  
+end
+

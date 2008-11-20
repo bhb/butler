e0a1cd598008d99a63e775a251638ef7c65ba4d3
Ben Brinckerhoff <ben@devver.net>
2008-11-19 22:40:51 -0700

    Added a view

--- iphone_doc.rb
+++ iphone_doc.rb
@@ -1,5 +1,4 @@
 #! /usr/bin/env ruby
-
 require 'sinatra'
 
 get "/" do
@@ -13,6 +12,19 @@ get "/" do
   out+="</big>"*bigness
 end
 
+get "/foo" do
+  dir = "./public/"
+  #bigness = 5
+  #out = "<big>"*bigness
+  #out += "<h3 style='+3'>your files</h3>"
+  @links = Dir[dir+"*"].map { |file| 
+    make_link(dir,file)
+  }
+  erb :index
+  #out+="</big>"*bigness
+  
+end
+
 helpers do
 
   def make_link(dir,file)
@@ -22,3 +34,24 @@ helpers do
   
 end
 
+use_in_file_templates!
+
+__END__
+
+@@ index
+<html>
+  <head>
+    <style type="text/css">
+      h1 {font-size: 300%}
+      p {font-size: 300%}
+    </style>
+  </head>
+
+<body>
+  <h1>Your files, sir. Enjoy.</h1>
+  <p>
+    <%= @links %>
+  </p>
+</body>
+
+</html>
